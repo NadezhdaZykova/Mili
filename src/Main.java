ad2a2065b1f724c374e54a9cbc179872965b6642
NadezhdaZykova <xilda777@gmail.com>
2021-03-11 21:01:21 +0400

    Mili

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        long price = 25000;
+        int count = 20;
+        long Milies = price / count;
+        System.out.println (Milies);
+    }
+}
